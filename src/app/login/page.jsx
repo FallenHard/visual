50e10955fd91124d08ba877629658bac4307aadb
maccioni <maccionidias@gmail.com>
2025-11-10 15:21:42 -0300

    AtualizaçãoDocker

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import "../../barber.css"
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5069"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://barbeariasite.onrender.com"
 
 export default function Login() {
     const [email, setEmail] = useState("")
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import "../../barber.css"
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://localhost:7037"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://barbeariasite.onrender.com"
 
 
 function Register() {
